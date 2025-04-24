af4c54a61420247a7b54625e2737ecdd41b63c61
seggitariusAstar <145839824+seggitariusAstar@users.noreply.github.com>
2025-04-24 19:04:22 +0530

    Update SendMoney.jsx
    updating the api to hit

--- frontend/src/Pages/SendMoney.jsx
+++ frontend/src/Pages/SendMoney.jsx
@@ -40,7 +40,7 @@ export const SendMoney = () => {
                     />
                     </div>
                     <button onClick={()=>{
-                        axios.post("http://localhost:3000/api/v1/account/transfer",{
+                        axios.post("https://payment-web-app-backend.onrender.com/api/v1/account/transfer",{
                             to:id,
                             amount:amount
                         },{
